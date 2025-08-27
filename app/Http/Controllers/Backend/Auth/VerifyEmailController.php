b09eabd1d0828c62154ff6bc961ebaeda2f96061
Xanders80 <xanders80@gmail.com>
2025-08-27 07:03:28 -0400

    Actualización y Revisión de Versión

--- app/Http/Controllers/Backend/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Backend/Auth/VerifyEmailController.php
@@ -3,54 +3,28 @@
 namespace App\Http\Controllers\Backend\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class VerifyEmailController extends Controller
 {
     /**
-     * Maneja la verificación de correo electrónico para el usuario autenticado.
+     * Mark the authenticated user's email address as verified.
      *
-     * @param EmailVerificationRequest $request
-     *
-     * @return RedirectResponse
+     * @param  \Illuminate\Foundation\Auth\EmailVerificationRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function __invoke(Request $request, $id, $hash)
+    public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
-        // Buscar al usuario por ID
-        $user = User::find($id); // Asumiendo que el ID del usuario se pasa como parámetro en la ruta
-
-        // Inicializar la variable para el mensaje de error
-        $errorMessage = null;
-
-        // Verificar si el usuario existe
-        if (!$user) {
-            $errorMessage = trans('Data not found');
-        } elseif ($user->email_verified_at !== null) {
-            abort(403, trans('mail already verified.'));
-        } else {
-            // Intentar marcar el correo electrónico como verificado
-            try {
-                if ($user->markEmailAsVerified()) {
-                    event(new Verified($user)); // Disparar el evento de verificación
-
-                    // Redirigir al dashboard con un parámetro de verificación
-                    return redirect()->intended(route('dashboard', false) . '?verified=1');
-                }
-            } catch (\Exception $e) {
-                // Registrar el error
-                $errorMessage = trans(config('constants.MESSAGES.DATA_ERROR')) . trans(config('constants.MESSAGES.ERROR_TRYING_TO_VERIFY_EMAIL')) . ' data: ' . $e->getMessage();
-                Log::error($errorMessage);
-            }
+        if ($request->user()->hasVerifiedEmail()) {
+            return redirect()->intended(route('dashboard', absolute: false) . '?verified=1');
         }
 
-        // Si hay un mensaje de error, redirigir con el mensaje correspondiente
-        if ($errorMessage) {
-            return $this->help::jsonResponse(false, $errorMessage, config('constants.STATUS_CODES.INTERNAL_SERVER_ERROR'));
+        if ($request->user()->markEmailAsVerified()) {
+            event(new Verified($request->user()));
         }
+
+        return redirect()->intended(route('dashboard', absolute: false) . '?verified=1');
     }
 }
